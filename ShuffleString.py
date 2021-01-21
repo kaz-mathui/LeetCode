1ba3ab48a017c7bfd3ccb7c1f663cd00f4877482
kaz-mathui <kazmat.hui@gmail.com>
2021-01-21 22:41:39 +0900

    add oneLine code

--- ShuffleString.py
+++ ShuffleString.py
@@ -1,10 +1,15 @@
+# class Solution:
+#     def restoreString(self, s: str, indices: List[int]) -> str:
+#         afterS = ['']*len(s)
+#         for i,j in enumerate(s):
+#             print(i,j)
+#             print(indices[i])
+#             afterS[indices[i]] = j
+#             print(afterS)
+#         return ''.join(afterS)
+            
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        afterS = ['']*len(s)
-        for i,j in enumerate(s):
-            print(i,j)
-            print(indices[i])
-            afterS[indices[i]] = j
-            print(afterS)
-        return ''.join(afterS)
-            
+        print(sorted(zip(indices, s)))
+        # return "".join(c for _, c in sorted(zip(indices, s)))
+        return ''.join(j for i,j in sorted(zip(indices,s)))
