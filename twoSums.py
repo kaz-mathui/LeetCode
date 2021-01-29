88e50435557eaf153ea6bc1a24f79d44a8d29fed
kaz-mathui <kazmat.hui@gmail.com>
2021-01-29 21:02:57 +0900

    最初の

--- twoSums.py
+++ twoSums.py
@@ -1,9 +1,16 @@
 class Solution:
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     result = []
+    #     for i in range(len(nums)-1):
+    #         for j in range(i+1,len(nums)):
+    #             if target == nums[i] + nums[j]:
+    #                 result.append(i)
+    #                 result.append(j)
+    #     return result
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result = []
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if target == nums[i] + nums[j]:
-                    result.append(i)
-                    result.append(j)
-        return result
+    hash_table = {}
+    for i in range(len(nums)):
+        if nums[i] in hash_table:
+            return [hash_table[nums[i]], i]
+        else:
+            hash_table[target - nums[i]] = i
