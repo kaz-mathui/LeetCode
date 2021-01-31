8fc2f0e5b86a945802b979da47e1513c7152a68d
kaz-mathui <kazmat.hui@gmail.com>
2021-01-31 22:52:58 +0900

    メアドユニーク

--- UniqueEmailAddress.py
+++ UniqueEmailAddress.py
@@ -1,16 +1,25 @@
-class Solution:
-    def numUniqueEmails(self, emails: List[str]) -> int:
-        hash_map = []
-        count = 0
-        for email in emails:
-            first = email[:email.find('@')+1]
-            first = first[:first.find('+')].replace('.','')
-            second = email[email.find('@')+1:]
-            print(first,second)
-            if first + '@' + second in hash_map:
-                continue
-            else:
-                hash_map.append(first + '@' + second)
-                count += 1
-        return count
+# class Solution:
+#     def numUniqueEmails(self, emails: List[str]) -> int:
+#         hash_map = []
+#         count = 0
+#         for email in emails:
+#             first = email[:email.find('@')+1]
+#             first = first[:first.find('+')].replace('.','')
+#             second = email[email.find('@')+1:]
+#             print(first,second)
+#             if first + '@' + second in hash_map:
+#                 continue
+#             else:
+#                 hash_map.append(first + '@' + second)
+#                 count += 1
+#         return count
             
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        seen = set()
+        for email in emails:
+            local, domain = email.split('@')
+            if '+' in local:
+                local = local[:local.index('+')]
+            seen.add(local.replace('.','') + '@' + domain)
+        return len(seen)
