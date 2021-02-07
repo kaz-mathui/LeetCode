fd8005b2df14b87d53f2c570f61b30096d1b4507
kaz-mathui <kazmat.hui@gmail.com>
2021-02-07 17:34:07 +0900

    しんどかった。ユニークパス２

--- uniquePath.py
+++ uniquePath.py
@@ -1,22 +1,66 @@
-def uniquePaths(grid: List[List[int]]) -> int:
-    m = len(grid)+1
-    n = len(grid[0])+1
-    result_list = []
-    result_list.append([1] * n)
-    for i in range(1,m):
-        n_list = []
-        n_list.append(1)
-        for j in range(1,n):
-            if grid[i-1][j-1] == 1:
-                n_list.append(0)
-            else:
-                n_list.append(result_list[i-1][j]+n_list[j-1])
-        result_list.append(n_list)
-    return result_list[m-1][n-1]
-    # if m == 1 or n == 1:
-    #     return 1
-        
-    # return uniquePaths(m - 1, n) + uniquePaths(m, n - 1)
+# class Solution:
+#     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+#         m = len(obstacleGrid)+1
+#         n = len(obstacleGrid[0])+1
+#         result_list = []
+#         result_list.append([1] * n)
+#         count = 0
+#         # print(result_list)
+#         for i in range(m):
+#             n_list = []
+#             n_list.append(1)
+#             for j in range(n):
+#                 if i == 0 or j == 0:
+#                     n_list.append(1)
+#                 elif obstacleGrid[i-1][j-1] == 1:
+#                     n_list.append(0)
+#                     # count +=1
+#                 # elif (i== 0 or j == 0) and obstacleGrid[i-1][j-1] == 0:
+#                 #     result_list[i][j] = 1
+#                 else:
+#                     n_list.append(result_list[i-1][j]+n_list[j-1])
+#                 # print(result_list)
+#             result_list.append(n_list)
+#         # print(m,n)
+#         print(result_list)
+#         return result_list[m-1][n-1]
 
-if __name__ == '__main__':
-    print(uniquePaths(23,12))
+
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+
+        # If the starting cell has an obstacle, then simply return as there would be
+        # no paths to the destination.
+        if obstacleGrid[0][0] == 1:
+            return 0
+
+        # Number of ways of reaching the starting cell = 1.
+        obstacleGrid[0][0] = 1
+
+        # Filling the values for the first column
+        for i in range(1,m):
+            obstacleGrid[i][0] = int(obstacleGrid[i][0] == 0 and obstacleGrid[i-1][0] == 1)
+
+        # Filling the values for the first row        
+        for j in range(1, n):
+            obstacleGrid[0][j] = int(obstacleGrid[0][j] == 0 and obstacleGrid[0][j-1] == 1)
+
+        # Starting from cell(1,1) fill up the values
+        # No. of ways of reaching cell[i][j] = cell[i - 1][j] + cell[i][j - 1]
+        # i.e. From above and left.
+        for i in range(1,m):
+            for j in range(1,n):
+                if obstacleGrid[i][j] == 0:
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+                else:
+                    obstacleGrid[i][j] = 0
+
+        # Return value stored in rightmost bottommost cell. That is the destination.            
+        return obstacleGrid[m-1][n-1]
