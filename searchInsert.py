0fc15be294932d19b8a86299cdb637456471e5e3
kaz-mathui <kazmat.hui@gmail.com>
2021-02-10 05:03:49 +0900

    バイナリサーチ

--- searchInsert.py
+++ searchInsert.py
@@ -1,8 +1,20 @@
+# class Solution:
+#     def searchInsert(self, nums: List[int], target: int) -> int:
+#         if target in nums:
+#             return nums.index(target)
+#         else:
+#             nums.append(target)
+#             nums.sort()
+#             return nums.index(target)
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
-        else:
-            nums.append(target)
-            nums.sort()
-            return nums.index(target)
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            pivot = (left + right) // 2
+            if nums[pivot] == target:
+                return pivot
+            if target < nums[pivot]:
+                right = pivot - 1
+            else:
+                left = pivot + 1
+        return left
