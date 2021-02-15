186fe19ccbed27f13804e9d3dca65f5c0a7b030d
kaz-mathui <kazmat.hui@gmail.com>
2021-02-15 09:18:10 +0900

    直感的

--- Single Number.py
+++ Single Number.py
@@ -1,16 +1,30 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-#         hash_map = []
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+# #         hash_map = []
         
-#         for i in nums:
-#             if i in hash_map:
-#                 continue
-#             if i in nums:
-#                 hash_map.append(i)
-#             else:
-#                 return i
+# #         for i in nums:
+# #             if i in hash_map:
+# #                 continue
+# #             if i in nums:
+# #                 hash_map.append(i)
+# #             else:
+# #                 return i
 
-        ans = 0
-        for n in nums:
-            ans ^= n
-        return ans
+#         ans = 0
+#         for n in nums:
+#             ans ^= n
+#         return ans
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        no_duplicate_list = []
+        for i in nums:
+            if i not in no_duplicate_list:
+                no_duplicate_list.append(i)
+            else:
+                no_duplicate_list.remove(i)
+        return no_duplicate_list.pop()
