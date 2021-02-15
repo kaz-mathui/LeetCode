3cbfbfb09eef43bb8cafbaa727d582af7bb5d1d3
kaz-mathui <kazmat.hui@gmail.com>
2021-02-15 22:43:10 +0900

    数列からないものを探す

---
+++ Find All Numbers Disappeared in an Array.py
@@ -0,0 +1,57 @@
+# class Solution:
+#     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        # result = []
+        # num = set(sorted(nums))
+        # print(num)
+        # for i in range(1,len(nums)+1):
+        #     print(i)
+        #     if i not in num:
+        #         result.append(i)
+        # return result
+        
+        
+#         hash_table = {}
+        
+#         for num in nums:
+#             hash_table[num] = 1
+        
+#         result = []
+        
+#         for num in range(1,len(nums)+1):
+#             if num not in hash_table:
+#                 result.append(num)
+        
+#         return result
+        
+    
+
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        # Iterate over each of the elements in the original array
+        for i in range(len(nums)):
+            
+            # Treat the value as the new index
+            new_index = abs(nums[i]) - 1
+            
+            # Check the magnitude of value at this new index
+            # If the magnitude is positive, make it negative 
+            # thus indicating that the number nums[i] has 
+            # appeared or has been visited.
+            if nums[new_index] > 0:
+                nums[new_index] *= -1
+        
+        # Response array that would contain the missing numbers
+        result = []    
+        
+        # Iterate over the numbers from 1 to N and add all those
+        # that have positive magnitude in the array 
+        for i in range(1, len(nums) + 1):
+            if nums[i - 1] > 0:
+                result.append(i)
+                
+        return result        
