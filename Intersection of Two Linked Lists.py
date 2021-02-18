68b0d80c63b8ccd82faf660ba022e9dc83a142a7
kaz-mathui <kazmat.hui@gmail.com>
2021-02-18 23:10:16 +0900

    hash以外はよくわかりません。共通するリンクリスト

---
+++ Intersection of Two Linked Lists.py
@@ -0,0 +1,37 @@
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        nodes_in_B = set()
+
+        while headB is not None:
+            nodes_in_B.add(headB)
+            headB = headB.next
+
+        while headA is not None:
+            # if we find the node pointed to by headA,
+            # in our set containing nodes of B, then return the node
+            if headA in nodes_in_B:
+                return headA
+            headA = headA.next
+
+        return None
+    
+    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# class Solution:
+#     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+#         if headA == None or headB == None:
+#             return None
+
+#         LNA = headA
+#         LNB = headB
+#         while LNA != LNB:
+#             LNA = headB if LNA == None else LNA.next
+#             LNB = headA if LNB == None else LNB.next
+#         return LNA
+# # Runtime: 168 ms, faster than 75.08% of Python3 online submissions for Intersection of Two Linked Lists.
+# # Memory Usage: 29.1 MB, less than 100.00% of Python3 online submissions for Intersection of Two Linked Lists.
