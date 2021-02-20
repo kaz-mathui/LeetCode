e76363fb41576a547bf3867635d4b744f430ffcd
kaz-mathui <kazmat.hui@gmail.com>
2021-02-20 17:02:33 +0900

    その場所までの和のリスト

---
+++ Running Sum of 1d Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        lists = []
+        ans = 0
+        for i in range(len(nums)):
+            ans += nums[i]
+            lists.append(ans)
+        return lists
