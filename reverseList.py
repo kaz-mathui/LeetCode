e9df6240f761f290795753b509e6e40bea80e497
kaz-mathui <kazmat.hui@gmail.com>
2021-02-21 12:21:50 +0900

    stackで反転させる

--- reverseList.py
+++ reverseList.py
@@ -1,25 +1,47 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def reverseList(self, head: ListNode) -> ListNode:
+#         prev = None
+#         curr = head
+#         while curr:
+#             next_node = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = next_node
+#         return prev
+        
+        
+        
+# #         if head == None or head.next == None:
+# #             return head
+# #         p = self.reverseList(head.next)
+# #         head.next.next = head
+# #         head.next = None
+# #         return p
+    
+    
+    
+    
+    
+    
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        prev = None
-        curr = head
-        while curr:
-            next_node = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_node
-        return prev
-        
-        
-        
-#         if head == None or head.next == None:
-#             return head
-#         p = self.reverseList(head.next)
-#         head.next.next = head
-#         head.next = None
-#         return p
-    
+        if not head:
+            return None
+
+        stack = []
+
+        while head.next:
+            stack.append(head)
+            head = head.next
+
+        while stack:
+            cur = stack.pop()
+            cur.next.next = cur
+            cur.next = None
+
+        return head
