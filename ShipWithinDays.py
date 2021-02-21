ed06c24cf6eae1bc7cfa81913b7ad549b9b2c035
kaz-mathui <kazmat.hui@gmail.com>
2021-02-21 18:16:00 +0900

    2分探索の問題。かなりわかりやすくなったけど、最後にトップとぼとむのどちらを答えにすればいいのかいつもテストケース任せ

---
+++ ShipWithinDays.py
@@ -0,0 +1,32 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], D: int) -> int:
+        
+        def numberOfDaysNeededWithCapacity(weights: List[int],mid:int) -> int:
+            daysRequired = 1
+            curr = 0
+            
+            for w in weights:
+                curr += w
+                if curr > mid:
+                    daysRequired += 1
+                    curr = w
+            return daysRequired
+        
+        
+        
+        bottom = max(weights)
+        top = sum(weights)
+        ans = 0
+        while bottom <= top:
+            mid = (bottom + top)//2
+            days = numberOfDaysNeededWithCapacity(weights,mid)
+            curr_weight = 0
+            
+            if days > D:
+                bottom = mid + 1
+            else:
+                top = mid - 1
+
+        return top + 1
+        
+        
