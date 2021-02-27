ae0525a06620f9a3718b1a5e54ae21b5b1ca05a0
kaz-mathui <kazmat.hui@gmail.com>
2021-02-27 17:14:47 +0900

    一個前の逆になっている。

--- KthGrammer.py
+++ KthGrammer.py
@@ -1,13 +1,8 @@
-class Solution(object):
-    def kthGrammar(self, N, K):
+class Solution:
+    def kthGrammar(self, N: int, K: int) -> int:
         if N == 1: return 0
-        if K <= (2**(N-2)):
+        half = 2**(N-2)
+        if K <= half:
             return self.kthGrammar(N-1, K)
-        return self.kthGrammar(N-1, K - 2**(N-2)) ^ 1
-# class Solution(object):
-#     def kthGrammar(self, N, K):
-#         lastrow = '0'
-#         while len(lastrow) < N:
-#             lastrow = "".join('01' if x == '0' else '10'
-#                               for x in lastrow)
-#         return int(lastrow[-1][K-1])
+        else:
+            return 1 if self.kthGrammar(N-1, K-half) == 0 else 0
