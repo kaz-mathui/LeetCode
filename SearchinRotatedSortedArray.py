66f3fbac0d055ccb1e67a40e61e760bd1981b62d
kaz-mathui <kazmat.hui@gmail.com>
2021-02-27 14:49:10 +0900

    順番バラバラな二分たんさ。

--- SearchinRotatedSortedArray.py
+++ SearchinRotatedSortedArray.py
@@ -1,38 +1,19 @@
-class Solution:
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        if not nums:
-            return -1
-        i = 0
-        j = len(nums)-1
-        while i < j:
-            mid = (i + j) // 2
-            if nums[mid] == target:
-                return mid
-            if nums[i] == target:
-                return i
-            if nums[j] == target:
-                return j
-            elif nums[mid] > target:
-                if target > nums[i]:
-                    j = mid-1
-                else:
-                    if nums[mid] >= nums[i]:
-                        i = mid+1
-                    else:
-                        j = mid-1
-            else:
-                if target < nums[i]:
-                    i = mid+1
-                else:
-                    if nums[mid] >= nums[i]:
-                        i = mid+1
-                    else:
-                        j = mid-1
-        if nums[i] == target:
-            return i
-        return -1
+def search(self, nums: List[int], target: int) -> int:
+	n = len(nums)
+	left, right = 0, n-1
+	while left <= right:
+		mid = (left + right) // 2
+		if nums[mid] == target:
+			return mid
+
+		if nums[left] <= nums[mid]:
+			if nums[left] <= target <= nums[mid]:
+				right = mid-1
+			else:
+				left = mid+1
+		else:
+			if nums[mid] <= target <= nums[right]:
+				left = mid+1
+			else:
+				right = mid-1  
+	return -1
